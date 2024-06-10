3f6364b9cd95e90077dd32d34650dbb216cb8c5f
askarsaparov <asqarsaparov2303@gmail.com>
2024-06-10 17:22:32 +0500

    last com

--- app.py
+++ app.py
@@ -15,26 +15,28 @@ def hello_world():
 @app.route('/', methods=['POST'])
 def handle_modbus():
     # Get the JSON data from the POST request
-    data = request.json
-    print(f"Received data: {data}")
+    print("request.data: ", request.data)
+    data = request.data
+    # data = request.json
 
+    print(f"Received data: {data}")
     dataclasses.append(data)
 
     # Extract the fields from the JSON payload
-    ip = data.get('ip')
-    port = data.get('port')
-    unit_id = data.get('unit_id')
-    address = data.get('address')
-    values = data.get('values')  # The Modbus register values
-    timestamp = data.get('timestamp', 'N/A')  # Optional timestamp
+    # ip = data.get('ip')
+    # port = data.get('port')
+    # unit_id = data.get('unit_id')
+    # address = data.get('address')
+    # values = data.get('values')  # The Modbus register values
+    # timestamp = data.get('timestamp', 'N/A')  # Optional timestamp
 
     # Process the data as needed
-    print(f"IP: {ip}")
-    print(f"Port: {port}")
-    print(f"Unit ID: {unit_id}")
-    print(f"Address: {address}")
-    print(f"Values: {values}")
-    print(f"Timestamp: {timestamp}")
+    # print(f"IP: {ip}")
+    # print(f"Port: {port}")
+    # print(f"Unit ID: {unit_id}")
+    # print(f"Address: {address}")
+    # print(f"Values: {values}")
+    # print(f"Timestamp: {timestamp}")
 
     # Implement your processing logic here
 
